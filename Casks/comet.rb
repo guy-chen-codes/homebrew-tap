5aae7f2c6b1d86add09b0f962b5517d854fed807
Guy Chen <guy@MBP-M1.local>
2025-10-26 13:11:05 +1100

    fix tap style

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -4,7 +4,7 @@ cask "comet" do
 
   url "https://www.perplexity.ai/rest/browser/binaries/#{version}/comet_latest.dmg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=9d06dad57704bf499ceb71a8730b22e4%2F20251025%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20251025T215511Z&X-Amz-Expires=300&X-Amz-Signature=8b9b75db46d4c7dbd392f0669fbc7a6769be31096d62e16c5dace01e268609f4&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
   name "comet"
-  desc "A Chromium-based browser that integrates AI capabilities powered by Perplexity."
+  desc "Chromium-based browser that integrates AI capabilities powered by Perplexity"
   homepage "https://www.perplexity.ai/comet"
 
   livecheck do
@@ -12,12 +12,11 @@ cask "comet" do
   end
 
   auto_updates false
-
   depends_on macos: ">= :monterey"
 
   app "Comet.app"
 
-  # No zap stanza found via `nrlquaker/createzap` 
+  # No zap stanza found via `nrlquaker/createzap`
   # Recommend using `AppCleaner.app` for uninstalling
   # zap trash: ""
 end
--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -3,7 +3,6 @@ cask "iterm2" do
   sha256 "82ee82369d14d452c2d3c8886d460e2466e46dc517eb3d96b2b04c97b0c23d49"
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
-  
   name "iTerm2"
   desc "Terminal emulator as alternative to Apple's Terminal app"
   homepage "https://iterm2.com/"
