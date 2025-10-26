030ceb9c574d6666daf0056c9f062d6aafdf0cfa
Guy Chen <guy@MBP-M1.local>
2025-10-26 12:48:15 +1100

    fix comet desc

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -4,7 +4,7 @@ cask "comet" do
 
   url "https://www.perplexity.ai/rest/browser/binaries/#{version}/comet_latest.dmg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=9d06dad57704bf499ceb71a8730b22e4%2F20251025%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20251025T215511Z&X-Amz-Expires=300&X-Amz-Signature=8b9b75db46d4c7dbd392f0669fbc7a6769be31096d62e16c5dace01e268609f4&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
   name "comet"
-  desc "Comet is a Chromium-based browser that integrates advanced AI capabilities powered by Perplexity. Unlike traditional browsers, Comet combines web browsing with intelligent features that enhance productivity and information access.\nComet also includes standard browser features like bookmarks, page translation, and support for most Chrome extensions."
+  desc "A Chromium-based browser that integrates AI capabilities powered by Perplexity."
   homepage "https://www.perplexity.ai/comet"
 
   livecheck do
