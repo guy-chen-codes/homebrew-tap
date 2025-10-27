7f9b1e65a6c1deedb30f8d360b00781f6de7541c
Guy <guy.chen.codes@gmail.com>
2025-10-28 09:10:20 +1100

    style fix

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,11 +1,10 @@
 cask "comet" do
   version "latest"
-  sha256 :no_check  # Since URL changes each run
+  sha256 :no_check # Since URL changes each run
 
   url ENV.fetch("HOMEBREW_CASK_DOWNLOAD_URL") do
     verified "perplexity.ai/"
   end
-
   name "Comet"
   desc "Chromium-based browser with integrated AI features by Perplexity"
   homepage "https://www.perplexity.ai/comet"
