f98ef18b774c96f20957f8b4823e161ab5165eaa
Guy Chen <107745757+guy-chen-codes@users.noreply.github.com>
2025-10-28 06:45:51 +1100

    Modify Comet cask for dynamic url
    Updated Comet cask to use latest version and dynamic URL.

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,22 +1,29 @@
 cask "comet" do
-  version "141.0.7390.23964"
-  sha256 "934fc0fda211bc692dc62b73845cc2544872e6f195fa75ef6dd52a2cdc2103b2"
+  version "latest"
+  sha256 :no_check  # Since URL changes each run
 
-  url "https://www.perplexity.ai/rest/browser/binaries/#{version}/comet_latest.dmg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=9d06dad57704bf499ceb71a8730b22e4%2F20251025%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20251025T215511Z&X-Amz-Expires=300&X-Amz-Signature=8b9b75db46d4c7dbd392f0669fbc7a6769be31096d62e16c5dace01e268609f4&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
-  name "comet"
-  desc "Chromium-based browser that integrates AI capabilities powered by Perplexity"
-  homepage "https://www.perplexity.ai/comet"
-
-  livecheck do
-    skip "Comet does not support livecheck mechanism"
+  url ENV.fetch("HOMEBREW_CASK_DOWNLOAD_URL") do
+    verified "perplexity.ai/"
   end
 
-  auto_updates false
+  name "Comet"
+  desc "Chromium-based browser with integrated AI features by Perplexity"
+  homepage "https://www.perplexity.ai/comet"
+
+  auto_updates true
   depends_on macos: ">= :monterey"
 
   app "Comet.app"
 
-  # No zap stanza found via `nrlquaker/createzap`
-  # Recommend using `AppCleaner.app` for uninstalling
-  # zap trash: ""
+  postflight do
+    puts "Comet installed successfully. 🚀"
+  end
+
+  uninstall quit: "ai.perplexity.comet"
+
+  zap trash: [
+    "~/Library/Application Support/Comet",
+    "~/Library/Preferences/ai.perplexity.comet.plist",
+    "~/Library/Saved Application State/ai.perplexity.comet.savedState",
+  ]
 end
