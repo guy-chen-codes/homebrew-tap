4b9d2c38da39d94358e4c35ad5398373120ce1d8
Guy Chen <guy.chen.codes@gmail.com>
2025-12-27 10:48:31 +1100

    add iloader

---
+++ Casks/iloader.rb
@@ -0,0 +1,21 @@
+cask "iloader" do
+  version "1.1.5"
+  sha256 :no_check
+
+  url "https://github.com/nab138/iloader/releases/download/v#{version}/iloader-darwin-universal.dmg"
+  name "iLoader"
+  desc "Firmware loader tool"
+  homepage "https://github.com/nab138/iloader"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "iLoader.app"
+
+  zap trash: [
+    "~/Library/Application Support/iLoader",
+    "~/Library/Saved Application State/com.flipperdevices.iloader.savedState",
+  ]
+end
