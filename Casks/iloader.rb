501786fa70597633e7d5143986eaa4d880ec2b02
Guy Chen <guy.chen.codes@gmail.com>
2025-12-30 17:16:54 +1100

    add sha256 check for iloader

--- Casks/iloader.rb
+++ Casks/iloader.rb
@@ -1,6 +1,6 @@
 cask "iloader" do
   version "1.1.6"
-  sha256 :no_check
+  sha256 "d59c3702f43772852f001776b5271563c885ee2f6e68d3811bc2624cfef9b8fc"
 
   url "https://github.com/nab138/iloader/releases/latest/download/iloader-darwin-universal.dmg"
   name "iLoader"
