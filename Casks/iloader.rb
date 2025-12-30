ac5818dc81fa9b2535e6e1555f1a870f1c9359c4
Guy Chen <guy.chen.codes@gmail.com>
2025-12-30 17:18:55 +1100

    fix iloader audit

--- Casks/iloader.rb
+++ Casks/iloader.rb
@@ -2,7 +2,7 @@ cask "iloader" do
   version "1.1.6"
   sha256 "d59c3702f43772852f001776b5271563c885ee2f6e68d3811bc2624cfef9b8fc"
 
-  url "https://github.com/nab138/iloader/releases/latest/download/iloader-darwin-universal.dmg"
+  url "https://github.com/nab138/iloader/releases/download/v#{version}/iloader-darwin-universal.dmg"
   name "iLoader"
   desc "Firmware loader tool"
   homepage "https://github.com/nab138/iloader"
