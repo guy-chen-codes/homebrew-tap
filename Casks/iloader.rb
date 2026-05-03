2bac5e0bf5979b2c435e38e71b5f71b828081c49
Guy Chen <guy.chen.codes@gmail.com>
2026-05-04 04:30:45 +1000

    update iloader to 2.2.5

--- Casks/iloader.rb
+++ Casks/iloader.rb
@@ -1,6 +1,6 @@
 cask "iloader" do
-  version "2.2.4"
-  sha256 "9e9954a517419250b852a0a359c437f182042b2f07ddaa19e3fb19855f472f37"
+  version "2.2.5"
+  sha256 "a01bf620cf91d701ec7935b0c6fc1fa0c3f1d9af5563e72164ed55293e0bfaf5"
 
   url "https://github.com/nab138/iloader/releases/download/v#{version}/iloader-darwin-universal.dmg"
   name "iLoader"
