4172611eca7c9952ee8e6806135d051a09f2474e
Guy Chen <guy.chen.codes@gmail.com>
2026-08-30 21:23:01 +1000

    update iloader to 2.3.1

--- Casks/iloader.rb
+++ Casks/iloader.rb
@@ -1,6 +1,6 @@
 cask "iloader" do
-  version "2.2.6"
-  sha256 "8494c0221b5fd3efbe7100637ecbeaddfe1bb5bf29de90b01a8b9de4a580cf1e"
+  version "2.3.1"
+  sha256 "91dc2b658fc1f4921dfb086e7799087153b48fd5925dad435fa90e564222298b"
   url "https://github.com/nab138/iloader/releases/download/v#{version}/iloader-darwin-universal.dmg"
   name "iLoader"
   desc "Firmware loader tool"
